95f97def70cd34a4ccf5e368629df72d3547606d
Vladyslav <MrBTTF>
2020-01-20 22:27:03 +0100

    Deploy on heroku

--- cyoa/cmd/server/main.go
+++ cyoa/cmd/server/main.go
@@ -1,51 +1,26 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"io/ioutil"
 	"encoding/json"
-	"log"
-	"path"
+	"fmt"
 	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
 )
 
-type ArcHandler struct {
-	arcs map[string]Arc
-	mux http.Handler
-}
-
-func NewArcHandler(arcs map[string]Arc) *ArcHandler{
-	return &ArcHandler{
-		arcs: arcs,
-	}
-}
-
-func (s *ArcHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_, arcName := path.Split(r.URL.Path)
-	arc, ok := s.arcs[arcName]
-	if !ok {
-		http.Redirect(w, r, "/intro", 301)
-	}
-	t, err := template.ParseFiles("arc.html")
-	if err != nil {
-		fmt.Println(err)
-	}
-    t.Execute(w, &arc)
-}
-
-
 type Option struct {
-	Text string
+	Text    string
 	NextArc string `json:"arc"`
 }
 
 type Arc struct {
-	Title string
-	Story []string
+	Title   string
+	Story   []string
 	Options []Option
 }
-
 
 func LoadBook(filename string) (map[string]Arc, error) {
 	jsonData, err := ioutil.ReadFile(filename)
@@ -59,17 +34,35 @@ func LoadBook(filename string) (map[string]Arc, error) {
 	return arcs, nil
 }
 
-func main()  {
-	fmt.Println("Choose Your Own Adventure server on port 8080")
+func main() {
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8080"
+	}
+	fmt.Printf("[INFO] \"Choose Your Own Adventure\" server is running on port %s\n", port)
 
 	arcs, err := LoadBook("assets/gopher.json")
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("[INFO] Story's arcs loaded successfully")
 
-	fmt.Println("arcs loaded successfully")
+	http.Handle("/", arcHandler(arcs))
+	http.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("./assets"))))
+	http.ListenAndServe(":"+port, nil)
+}
 
-	http.Handle("/",  NewArcHandler(arcs))
-	http.Handle("/css/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
-	http.ListenAndServe(":8080", nil)
+func arcHandler(arcs map[string]Arc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, arcName := path.Split(r.URL.Path)
+		arc, ok := arcs[arcName]
+		if !ok {
+			http.Redirect(w, r, "/intro", 301)
+		}
+		t, err := template.ParseFiles("assets/arc.html")
+		if err != nil {
+			fmt.Println(err)
+		}
+		t.Execute(w, &arc)
+	})
 }
