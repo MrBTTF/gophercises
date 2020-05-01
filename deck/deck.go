565deba8b9419813024e200add63341de740ba21
Vladyslav <MrBTTF>
2020-05-01 15:04:09 +0200

    Deck Of Cards

---
+++ deck/deck.go
@@ -0,0 +1,140 @@
+package deck
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+//go:generate stringer -type=Suit,Rank
+
+type Suit uint8
+
+const (
+	Spade Suit = iota
+	Diamond
+	Club
+	Heart
+	Joker
+)
+
+var suits = [...]Suit{Spade, Diamond, Club, Heart}
+
+type Rank uint8
+
+const (
+	_ Rank = iota
+	Ace
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+const (
+	minRank = Ace
+	maxRank = King
+)
+
+type Card struct {
+	Suit
+	Rank
+}
+
+func (c Card) String() string {
+	if c.Suit == Joker {
+		return c.Suit.String()
+	}
+	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
+}
+
+func New(options ...func([]Card) []Card) []Card {
+	var cards []Card
+
+	for _, suit := range suits {
+		for rank := minRank; rank <= maxRank; rank++ {
+			cards = append(cards, Card{Suit: suit, Rank: rank})
+		}
+	}
+	for _, opt := range options {
+		cards = opt(cards)
+	}
+	return cards
+}
+
+func Deck(n int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var result []Card
+		for i := 0; i < n; i++ {
+			result = append(result, cards...)
+		}
+		return result
+	}
+}
+
+func Filter(f func(card Card) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var result []Card
+		for _, c := range cards {
+			if !f(c) {
+				result = append(result, c)
+			}
+		}
+		return result
+	}
+}
+
+func Jokers(n int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		for i := 0; i < n; i++ {
+			cards = append(cards, Card{
+				Rank: Rank(i),
+				Suit: Joker,
+			})
+		}
+		return cards
+	}
+}
+
+var shuffleRand = rand.New(rand.NewSource(time.Now().Unix()))
+
+func Shuffle(cards []Card) []Card {
+	result := make([]Card, len(cards))
+	r := rand.New(shuffleRand)
+	perm := r.Perm(len(cards))
+	for i, j := range perm {
+		result[i] = cards[j]
+	}
+	return result
+}
+
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+func Sort(less func(cards []Card) func(i, j int) bool) func(cards []Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+func absRank(c Card) int {
+	return int(c.Suit)*int(maxRank) + int(c.Rank)
+}
---
+++ deck/go.mod
@@ -0,0 +1,9 @@
+module github.com/MrBTTF/gophercises/deck
+
+go 1.14
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/nathany/looper v0.3.3 // indirect
+	golang.org/x/sys v0.0.0-20200501052902-10377860bb8e // indirect
+)
